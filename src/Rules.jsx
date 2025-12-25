d082e6d94c5c12e9ab69bb4bf920e920727898db
sleeveartist <119133464+sleeveartist@users.noreply.github.com>
2025-12-25 22:54:41 +0300

    fix link

--- src/Rules.jsx
+++ src/Rules.jsx
@@ -58,6 +58,8 @@ export default function Rules() {
                     </div>
                 ))}
             </div>
+            <button onClick={() => navigate("/admin")}>Admin</button>
         </div>
     )
+
 }
